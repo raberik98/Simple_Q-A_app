5c24df4878139e7d5ca1f57e98e95afd3660a835
Erik <erik.raboczki98@gmail.com>
2022-09-27 12:52:38 +0200

    EditQuestion API added, I will make the DeleteQuestion too and then I will test it on the frontend GUI

--- backend/controllers/question.js
+++ backend/controllers/question.js
@@ -41,5 +41,40 @@ exports.postNewQuestion = () => {
 }
 
 exports.editQuestion = () => {
+    userId = userController.isLoggedIn()
+
+    if (!userId) {
+        const _id = req.params.questionId
+        if (_id) {
+            const newtitle = req.body.title
+            const newmessage = req.body.message
+            if (newmessage && newtitle) {
+                Question.findOne({_id}).then(() => {
+                    Question.updateOne({_id}, {title: newtitle}, {message: newmessage}).then(() => {
+                        return res.status(200).json({"error":"Success! Your question have been edited."})
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        return res.status(500).json({"error":"Database error, please try it again later."})
+                    })
+                })
+                .catch(() => {
+                    return res.status(404).json({"error":"Question doesn't exist."})
+                })
+            }
+            else{
+                return res.status(400).json({ "error": "Bad input!" });
+            }
+        }
+        else {
+            return res.status(400).json({ "error": "Bad request!" });
+        }
+    }
+    else{
+        return res.status(401).json({"error":"You are unauthorized to edit this question, please log in!"})
+    }
+}
+
+exports.deleteQuestion = () => {
     
 }
--- backend/routes/Question.js
+++ backend/routes/Question.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const questionController = require("../controllers/question")
 
 router.get('/api/getallquestions', questionController.getAllQuestions);
+router.post('/api/postnewquestion', questionController.postNewQuestion);
+router.post('/api/editquestion/:questionId', questionController.editQuestion);
+router.delete('/api/deletequestion/:questionId', questionController.deleteQuestion);
 
 module.exports = router;
